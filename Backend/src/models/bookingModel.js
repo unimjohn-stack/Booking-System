bd30cec32b4da9dce1c804602cd535eaebb0b0b6
unimjohn <johniduku@gmail.com>
2026-08-26 15:58:24 +0100

    booking model

---
+++ Backend/src/models/bookingModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+    business: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Business",
+        required: true,
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Customer",
+        required: true,
+    },
+    service: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Service",
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    startTime: {
+        type: String,
+        required: true,
+    },
+    endTime: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["Pending","Confirmed", "Completed", "Completed"],
+        default: "Pending",
+    },
+}, {
+    timestamps: true
+});
+
+const Booking = mongoose.model("Booking", bookingSchema);
+export default Booking;
