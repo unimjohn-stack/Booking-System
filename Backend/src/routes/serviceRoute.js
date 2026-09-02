4be7f237d2f9a58319340780961656cfcb45dee3
unimjohn <johniduku@gmail.com>
2026-09-02 16:47:21 +0100

    editing serviceRoute

--- Backend/src/routes/serviceRoute.js
+++ Backend/src/routes/serviceRoute.js
@@ -2,13 +2,13 @@ import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
 import { createService, deactivateService, getMyService, getMyServices, getService, getServices, updateService } from '../controllers/serviceController.js';
 
-const router = express.Router;
+const router = express.Router();
 
 router.post('/', protectRoute, createService);
-router.get('/', protectRoute, getMyServices);
-router.get('/:id', protectRoute, getMyService);
+router.get('/my', protectRoute, getMyServices);
+router.get('/my/:id', protectRoute, getMyService);
 router.patch('/:id', protectRoute, updateService);
-router.patch('/:id', protectRoute, deactivateService);
+router.patch('/:id/deactivate', protectRoute, deactivateService);
 
 router.get('/:id', getService);
 router.get('/', getServices)
