37f2c85a35f3dccbdc2d7a740289c47067b4239a
unimjohn <johniduku@gmail.com>
2026-09-02 15:58:48 +0100

    getMyService

--- Backend/src/controllers/serviceController.js
+++ Backend/src/controllers/serviceController.js
@@ -39,6 +39,26 @@ export const getMyServices = async (req, res) => {
     }
 }
 
+export const getMyService = async (req, res) => {
+    try {
+        const business = await Business.findOne({ owner: req.user._id });
+        if (!business) {
+            return res.status(404).json({ message: "Business not found" });
+        }
+        const service = await Service.findOne( req.params.id);
+        if (!service) {
+            return res.status(404).json({ message: "Service not found" });
+        }
+        if (service.business._id !== business._id) {
+            return res.status(403).json({ message: "This service does not belong to your business" });
+        }
+        return res.status(200).json({ succss: true, message: "Service found successfully", service, });
+    } catch (error) {
+        console.error("Error in getMyService Controller", error.message);
+        return res.status(500).json({ message: "Internal Server Error"});
+    }
+}
+
 export const getService = async (req, res) => {
     try {
         const service = await Service.findById(req.params.id);
