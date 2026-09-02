e287027382506aa15b6d4d7e1812adf5109ea850
unimjohn <johniduku@gmail.com>
2026-09-02 15:43:39 +0100

    getMyServices Controller

--- Backend/src/controllers/serviceController.js
+++ Backend/src/controllers/serviceController.js
@@ -21,3 +21,20 @@ export const createService = async (req, res) => {
         return res.status(500).json({ message: "Internal Server Error" });
     }
 }
+
+export const getMyServices = async (req, res) => {
+    try {
+        const business = await Business.findOne({ owner: req.user._id });
+        if (!business) {
+            return res.status(404).json({ message: "Business not found" });
+        }
+        const services = await Service.find({ business: business._id });
+        if (services.length === 0) {
+            return res.status(200).json({ success: true, message: "No Services for this Business yet", services: [], });
+        }
+        return res.status(200).json({ success: true, message: "Services found successfully", services });
+    } catch (error) {
+        console.error("Error in getMyServcices Controller:", error.message);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
